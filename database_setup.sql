3a8048cd4b5c687c560a018f40386f87b703cbbe
Greeshma Polanki <vu.241fa04776@gmail.com>
2026-06-02 22:02:27 +0530

    Create database and retail_sales table

---
+++ database_setup.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE p1_retail_db;
+USE p1_retail_db;
+
+CREATE TABLE retail_sales
+(
+    transaction_id INT PRIMARY KEY,
+    sale_date DATE,
+    sale_time TIME,
+    customer_id INT,
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,
+    cogs FLOAT,
+    total_sale FLOAT
+);
